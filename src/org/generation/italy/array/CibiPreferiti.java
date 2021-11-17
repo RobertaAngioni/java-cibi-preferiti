2f0173ca478ceaeab9c493c04cddc50bb19f6303
RobertaAngioni <robertaangioni92@gmail.com>
2021-11-17 14:52:24 +0100

    primo commit

---
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -0,0 +1,29 @@
+package org.generation.italy.array;
+
+public class CibiPreferiti {
+
+	public static void main(String[] args) {
+	String[] ciboPreferito = new String[7];
+        
+	//inizializzazione cibi
+	
+	ciboPreferito[0] = "calamari e piselli";
+	ciboPreferito [1] = "udon con gamberi e verdure";
+	ciboPreferito[2] = "linguine con sugo agli scampi";
+	ciboPreferito[3] = "sushi";
+	ciboPreferito[4] = "montanara";
+	ciboPreferito[5] = "zuppa agropiccante";
+	ciboPreferito[6] = "frittura di pesce";
+	
+		int lunghezzaArray = ciboPreferito.length;
+		System.out.println("Array di " + lunghezzaArray + " elementi");
+		
+		
+		
+		
+	}
+
+
+	}
+
+
